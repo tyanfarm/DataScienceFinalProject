4e766db953bc378224bae38ea030a770dcb29512
tyanfarm <phamquangtuyen.nt@gmail.com>
2024-01-07 14:26:46 +0700

    remane variable in service.py

--- deployment/service.py
+++ deployment/service.py
@@ -2,10 +2,10 @@ import numpy as np
 import bentoml
 from bentoml.io import NumpyNdarray
 
-universities_rank_neural_network_runner = bentoml.sklearn.get("universities_rank_regression:latest").to_runner()
-svc = bentoml.Service("university_rank",runners=[universities_rank_neural_network_runner])
+universities_rank_regression_runner = bentoml.sklearn.get("universities_rank_regression:latest").to_runner()
+svc = bentoml.Service("university_rank",runners=[universities_rank_regression_runner])
 
 @svc.api(input=NumpyNdarray(),output=NumpyNdarray())
 def predict_rank(input_series: np.ndarray) -> np.ndarray:
-    result = universities_rank_neural_network_runner.predict.run(input_series)
+    result = universities_rank_regression_runner.predict.run(input_series)
     return result
